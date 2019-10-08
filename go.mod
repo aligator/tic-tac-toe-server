e434886001226d49203398a4455db57888e17872
aligator <aligator@suncraft-server.de>
2019-10-08 21:21:54 +0200

    add cors headers

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/render v1.0.1
 )
--- server/routes.go
+++ server/routes.go
@@ -2,10 +2,23 @@ package server
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) mountRoutes() {
 	r := chi.NewRouter()
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
 
 	r.Route("/game", func(r chi.Router) {
 		r.Get("/winner", s.controller.GetWinner())
